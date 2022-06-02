fee257549d74c74effa997cd746f34330bf4b574
Nye-Pravda <105072523+Nye-Pravda@users.noreply.github.com>
2022-06-02 11:35:14 -0400

    Update part3.py
    amountnonzero must be added to keep track of the amount of inputs, if amountnonzero starts at zero it wont count zero as one input

--- part3.py
+++ part3.py
@@ -14,11 +14,15 @@ Enter a number or enter 0 to stop: 0
 Average: 6.0
 
 '''
-num = int(input("Enter a number or enter 0 to stop:"))
+num = int(input("Enter a number or enter 0 to stop: "))
 total = 0
+amountnonzero = 0
 
 while num != 0:
   total = total + num
-  num = int(input("Enter a number or enter 0 to stop:"))
+  num = int(input("Enter a number or enter 0 to stop: "))
+  amountnonzero += 1
+
+total = total / amountnonzero
     
 print("Average:", total)
